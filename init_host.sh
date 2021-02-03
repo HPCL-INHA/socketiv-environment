29e437ae895eb2e9f0876be099ecf2a1da23c012
minmossi <alswg1994@gmail.com>
2021-02-03 15:01:28 +0900

    update

--- init_host.sh
+++ init_host.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Initializing..."
+sudo killall -9 ivshmem-server
 sudo rm -f /tmp/ivshmem_socket
 sudo -b nohup ivshmem-server -Fv -m /dev/hugepages -l 1G </dev/null >/dev/null 2>&1 &
+echo "Initializing..."
 sleep 5
 sudo chmod 777 /tmp/ivshmem_socket
